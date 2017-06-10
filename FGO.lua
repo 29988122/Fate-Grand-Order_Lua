00f6eb88bfe60ae10ff4c8edd5a133d23f941d90
Jerry Ho 何建霖 <a29988122@gmail.com>
2017-06-10 14:51:56 +0800

    Functionality fix
    -Now it correctly skips past "reward" screen, better daily freequest
    automation
    -Will automatically select either "servant" or "danger", save you some
    time.
    -comment cleanup

--- FGO.lua
+++ FGO.lua
@@ -10,6 +10,8 @@ sNpbar = Region(280,1330,1620,50)
 sTarget1 = Region(0,0,485,220)
 sTarget2 = Region(485,0,482,220)
 sTarget3 = Region(967,0,476,220)
+sQuestreward = Region(1630,140,370,250)
+questreward = (Location(100,100))
 card1 = (Location(300,1000))
 card2 = (Location(750,1000))
 card3 = (Location(1300,1000))
@@ -19,26 +21,22 @@ target1 = (Location(90,80))
 target2 = (Location(570,80))
 target3 = (Location(1050,80))
 
-Settings:setCompareDimension(true, 1280)
-Settings:setScriptDimension(true, 2560)
-
-atkround = 1
---Settings:set("MinSimilarity", 0.6)
---Settings:set("AutoWaitTimeout", 1)
-
 --ultcard1 = Region(900,100,200,200)
 --ultcard2 = Region(1350,100,200,200)
 --ultcard3 = Region(1800,100,200,200)
 
---[[1.weak改顏色
-
-speed:
+Settings:setCompareDimension(true, 1280)
+Settings:setScriptDimension(true, 2560)
+atkround = 1
+--[[
+recognize speed realated functions:
 1.setScanInterval(1)
 2.Settings:set("MinSimilarity", 0.5)
 3.Settings:set("AutoWaitTimeout", 1)
 4.usePreviousSnap(true)
 5.resolution 1280
 6.exists(var ,0)]]
+
 function menu()
     atkround = 1
     wait (1)
@@ -89,39 +87,39 @@ end
 
 function norcard()
     i = 0
-    f1 = sCard1:exists("weaknew.png")
+    f1 = sCard1:exists("weak.png")
 	usePreviousSnap(true)
     if f1 ~= nil then
         click(card1)
-        toast("card1")
+        --toast("card1")
         i = i + 1
     end
 
-    f2 = sCard2:exists("weaknew.png")
+    f2 = sCard2:exists("weak.png")
     if f2 ~= nil then
         click(card2)
-        toast("card2")
+        --toast("card2")
         i = i + 1
     end
 
-    f3 = sCard3:exists("weaknew.png")
+    f3 = sCard3:exists("weak.png")
     if f3 ~= nil then
         click(card3)
-        toast("card3")
+        --toast("card3")
         i = i + 1
     end
 
-    f4 = sCard4:exists("weaknew.png")
+    f4 = sCard4:exists("weak.png")
     if f4 ~= nil then
         click(card4)
-        toast("card4")
+        --toast("card4")
         i = i + 1
     end
 
-    f5 = sCard5:exists("weaknew.png")
+    f5 = sCard5:exists("weak.png")
     if f5 ~= nil then
         click(card5)
-        toast("card5")
+        --toast("card5")
         i = i + 1
     end
 	usePreviousSnap(false)
@@ -135,24 +133,27 @@ function ultcard()
 end
 
 function targetchoose()
-    t1 = sTarget1:exists("target.png")
+    t1 = sTarget1:exists("target_servant.png")
 	usePreviousSnap(true)
-	t2 = sTarget2:exists("target.png")
-	t3 = sTarget3:exists("target.png")
-    if t1 ~= nil then
+	t2 = sTarget2:exists("target_servant.png")
+	t3 = sTarget3:exists("target_servant.png")
+	t1a = sTarget1:exists("target_danger.png")
+	t2a = sTarget2:exists("target_danger.png")
+	t3a = sTarget3:exists("target_danger.png")
+    if t1 ~= nil or t1a ~= nil then
         click(target1)
-		toast("Switched to boss")
+		toast("Switched to priority target")
 		targetchoosen = 1
-	elseif t2 ~= nil then
+	elseif t2 ~= nil or t2a ~= nil then
 		click(target2)
-		toast("Switched to boss")
+		toast("Switched to priority target")
 		targetchoosen = 1
-	elseif t3~= nil then
+	elseif t3 ~= nil or t3a ~= nil then
 		click(target3)
-		toast("Switched to boss")
+		toast("Switched to priority target")
 		targetchoosen = 1
 	else
-		toast("No boss selected")
+		toast("No priority target selected")
     end
 	usePreviousSnap(false)
 end
@@ -164,9 +165,14 @@ function result()
     click(Location(1000, 1000))
     wait(3)
     click(Location(2200, 1350))
-    wait(3)
+    wait(10)
+	r1 = sQuestreward:exists("questreward.png")
+	if r1 ~= nil then
+		click(questreward)
+	end
 end
 
+--[[
 function berserkerbattle()
     click(Location(2300,1200))
     if atkround >= 4 then
@@ -180,13 +186,12 @@ function berserkerbattle()
     atkround = atkround + 1
     wait (3)
 end
+]]
 
 while(1) do
     if rMenu:exists("menu.png", 0) then
-		toast("Will only select servant as noble phantasm target, please check github for further detail")
+		toast("Will only select servant/danger enemy as noble phantasm target, please check github for further detail")
         menu()
-
-		
 		targetchoosen = 0
     end
     if rBattle:exists("battle.png", 0) then
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Fate-Grand-Order_Lua
-AS OF 2017.05.01 CCC EVENT, THIS SCRIPT IS WORKING WITHOUT ROOT / BEING BLOCKED, I DO NOT TAKE RESPONSIBILITY FOR YOUR BANNED ACCOUNT! (Alghough I highly doubt it, they are terrible coders, let alone this script run at human speed.)
+AS OF 2017.06.10, THIS SCRIPT IS WORKING WITHOUT ROOT / BEING BLOCKED, I DO NOT TAKE RESPONSIBILITY FOR YOUR BANNED ACCOUNT! (Alghough I highly doubt it, they are terrible coders, let alone this script run at human speed.)
 
 Screw those farming events - I only wanna enjoy the (kinoko) story!
 Therefore I brought you this: FGO automation script.
@@ -11,21 +11,21 @@ Download the latest version of sikuli framework here.
 2. Enable usb debugging, connect your PC to android and execute run.bat inside ama_daemon folder. After daemon has been installed, you can disable usb debugging and unplug. DAEMON NEEDS TO BE REINSTALLED UPON REBOOT.
 
 3. Open ankulua to check if daemon is properly installed - and you're done.
-Put your game in
-- menu, put your desired quest on the 1st item.
+Put your game in either:
+- menu, put your desired quest as the 1st item on the screen(upper-right corner).
 - battle screen.
 and click the transprent arrow. You're good to go.
 
 
 Behavior details:
-- It is custoimized for events that I'm playing, hence some minor adjustments.
-Most of the time it can run any quest without problem.
+- It is custoimized for events that I'm playing, hence some minor adjustments will be done frequently.
+But, most of the time it can run any quest without problem.
 
 - Stamina does NOT automatically refilled, you have to eat those precious apples manually.
 
 - Script automatically chooses weak cards, until "boss" fight.
-It will switch target to any "servant", cast Noble Phantasm immediately and after until battle ended.
-You can manually replace target.png inside image folder to customize your priority target.
+It will switch target to any "servant" or "danger" enemy, cast Noble Phantasm immediately and after until battle ended.
+You can manually replace target_servant.png inside image folder to customize your priority target.
 1. screenshot your phone
 2. convert it to png format
 3. resize it to 1280 WIDTH (1920*1080->1280*720, 2560*1440->1280*720, etc.)
