db32bde38fc3eb39e0e24869a41734d1460db068
TryBane <trybane@gmail.com>
2018-12-25 09:29:46 -0500

    Update resolution-scaling.lua

--- resolution-scaling.lua
+++ resolution-scaling.lua
@@ -1,13 +1,15 @@
 xOffset = 0
 yOffset = 0
 
-xDifferential = getAppUsableScreenSize():getX() / 2560
-yDifferential = getAppUsableScreenSize():getY() / 1440
+function createOffsets()
+        xDifferential = getAppUsableScreenSize():getX() / 2560
+        yDifferential = getAppUsableScreenSize():getY() / 1440
 
-if yDifferential > xDifferential then
-        yOffset = xDifferential * 1440 / 2
-elseif yDifferential < xDifferential then
-        xOffset = yDifferential * 2560 / 2
+        if yDifferential > xDifferential then
+                yOffset = xDifferential * 1440 / 2
+        elseif yDifferential < xDifferential then
+                xOffset = yDifferential * 2560 / 2
+        end
 end
 
 function getYOffset()
@@ -17,3 +19,10 @@ end
 function getXOffset()
         return xOffset
 end
+
+return
+{
+        createOffsets = createOffsets,
+        getYOffset = getYOffset,
+        getXOffset = getXOffset
+}
