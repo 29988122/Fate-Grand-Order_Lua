0f6906b20b4c30ec4f8c073ea7fc6d6af3c90391
TryBane <trybane@gmail.com>
2018-12-28 00:40:58 -0500

    Update FGO_EN_REGULAR.lua

--- FGO_EN_REGULAR.lua
+++ FGO_EN_REGULAR.lua
@@ -11,13 +11,11 @@ xDifferential = getAppUsableScreenSize():getX() / 2560
 yDifferential = getAppUsableScreenSize():getY() / 1440
 
 if yDifferential > xDifferential then
-    xOffset = ( getAppUsableScreenSize():getX() - ( yDifferential * 2560 ) ) / yDifferential / 2
     yOffset = ( getAppUsableScreenSize():getY() - ( xDifferential * 1440 ) ) / xDifferential / 2
     Settings:setCompareDimension(true,1280)
     Settings:setScriptDimension(true,2560)
 elseif yDifferential < xDifferential then
     xOffset = ( getAppUsableScreenSize():getX() - ( yDifferential * 2560 ) ) / yDifferential / 2
-    yOffset = ( getAppUsableScreenSize():getY() - ( xDifferential * 1440 ) ) / xDifferential / 2
     Settings:setCompareDimension(false,720)
     Settings:setScriptDimension(false,1440)
 else
