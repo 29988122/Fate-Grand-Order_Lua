4f8882a7350f1406a49092cfb6d4f68c1c865e87
TryBane <trybane@gmail.com>
2018-12-31 07:30:44 -0500

    Update FGO_CN_REGULAR.lua

--- FGO_CN_REGULAR.lua
+++ FGO_CN_REGULAR.lua
@@ -7,15 +7,15 @@ setImmersiveMode(true)
 xOffset = 0
 yOffset = 0
 
-xDifferential = getAppUsableScreenSize():getX() / 2560
-yDifferential = getAppUsableScreenSize():getY() / 1440
+xDifferential = getRealScreenSize():getX() / 2560
+yDifferential = getRealScreenSize():getY() / 1440
 
 if yDifferential > xDifferential then
-    yOffset = ( getAppUsableScreenSize():getY() - ( xDifferential * 1440 ) ) / xDifferential / 2
+    yOffset = ( getRealScreenSize():getY() - ( xDifferential * 1440 ) ) / xDifferential / 2
     Settings:setCompareDimension(true,1280)
     Settings:setScriptDimension(true,2560)
 elseif yDifferential < xDifferential then
-    xOffset = ( getAppUsableScreenSize():getX() - ( yDifferential * 2560 ) ) / yDifferential / 2
+    xOffset = ( getRealScreenSize():getX() - ( yDifferential * 2560 ) ) / yDifferential / 2
     Settings:setCompareDimension(false,720)
     Settings:setScriptDimension(false,1440)
 else
