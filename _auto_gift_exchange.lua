20714b56ef6f6dd27f5be97b3be9c77e0e0a5124
TryBane <trybane@gmail.com>
2019-01-07 20:40:23 -0500

    Update _auto_gift_exchange.lua
    Supporting other aspect ratios (simplified fix)

--- _auto_gift_exchange.lua
+++ _auto_gift_exchange.lua
@@ -1,16 +1,33 @@
 -- settings
+xOffset = 0
+yOffset = 0
+
+xDifferential = getAppUsableScreenSize():getX() / 2560
+yDifferential = getAppUsableScreenSize():getY() / 1440
+
+if yDifferential > xDifferential then
+    yOffset = ( getAppUsableScreenSize():getY() - ( xDifferential * 1440 ) ) / xDifferential / 2
+    Settings:setCompareDimension(true,1280)
+    Settings:setScriptDimension(true,2560)
+elseif yDifferential < xDifferential then
+    xOffset = ( getAppUsableScreenSize():getX() - ( yDifferential * 2560 ) ) / yDifferential / 2
+    Settings:setCompareDimension(false,720)
+    Settings:setScriptDimension(false,1440)
+else
+    Settings:setCompareDimension(true,1280)
+    Settings:setScriptDimension(true,2560)
+end
+
 setImmersiveMode(true)
-Settings:setCompareDimension(true, 1280)
-Settings:setScriptDimension(true, 1280)
 local dir = scriptPath()
 setImagePath(dir .. "image_EN")
 
 -- consts
-local SpinClick = Location(417, 430)
-local FinishedLotteryBoxRegion = Region(177, 411, 258, 151)
-local ResetClick = Location(1100, 240)
-local ResetConfirmationClick = Location(837, 561)
-local ResetCloseClick = Location(635, 560)
+local SpinClick = Location(417 + xOffset, 430 + yOffset)
+local FinishedLotteryBoxRegion = Region(177 + xOffset, 411 + yOffset, 258, 151)
+local ResetClick = Location(1100 + xOffset, 240 + yOffset)
+local ResetConfirmationClick = Location(837 + xOffset, 561 + yOffset)
+local ResetCloseClick = Location(635 + xOffset, 560 + yOffset)
 
 -- script
 local function spin()
