282317c2f2bd511394040d55e18efe49ba6d3a73
TryBane <trybane@gmail.com>
2019-01-07 20:38:59 -0500

    Update _auto_friendgacha.lua
    Supporting other aspect ratios (simplified fix)

--- _auto_friendgacha.lua
+++ _auto_friendgacha.lua
@@ -1,10 +1,27 @@
-Settings:setCompareDimension(true,1280)
-Settings:setScriptDimension(true,2560)
+xOffset = 0
+yOffset = 0
+
+xDifferential = getAppUsableScreenSize():getX() / 2560
+yDifferential = getAppUsableScreenSize():getY() / 1440
+
+if yDifferential > xDifferential then
+    yOffset = ( getAppUsableScreenSize():getY() - ( xDifferential * 1440 ) ) / xDifferential / 2
+    Settings:setCompareDimension(true,1280)
+    Settings:setScriptDimension(true,2560)
+elseif yDifferential < xDifferential then
+    xOffset = ( getAppUsableScreenSize():getX() - ( yDifferential * 2560 ) ) / yDifferential / 2
+    Settings:setCompareDimension(false,720)
+    Settings:setScriptDimension(false,1440)
+else
+    Settings:setCompareDimension(true,1280)
+    Settings:setScriptDimension(true,2560)
+end
+
 setImmersiveMode(true)
 while(1) do
-    click(Location(1600,1120))
-    click(Location(1600,1120))
+    click(Location(1600 + xOffset,1120 + yOffset))
+    click(Location(1600 + xOffset,1120 + yOffset))
     wait(3)
-    continueClick(Location(1660,1300), 15)
+    continueClick(Location(1660 + xOffset,1300 + yOffset), 15)
     wait(0.5)
 end
