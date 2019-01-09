453b3afd74b3d7cc1465ba3c19bacb1bd9d67158
potchy <potchy@protonmail.com>
2019-01-09 15:52:45 -0200

    Added "Round" function

--- modules/math-utils.lua
+++ modules/math-utils.lua
@@ -1,8 +1,15 @@
 local mathUtils = {}
 
+--  source: https://stackoverflow.com/a/36798347/4276186
 function mathUtils.Truncate(number)
-    local decimal_places = number % 1
-    return number - decimal_places
+    local decimalPlaces = number % 1
+    return number - decimalPlaces
+end
+
+-- source: http://lua-users.org/wiki/SimpleRound
+function mathUtils.Round(number)
+    local roundedAsString = string.format("%.0f", number)
+    return tonumber(roundedAsString)
 end
 
 return mathUtils
