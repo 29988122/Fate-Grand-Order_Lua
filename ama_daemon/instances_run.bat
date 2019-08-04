5fa356f747977d4ba76578372753f6e3afb8bc7b
potchy <potchy@protonmail.com>
2019-08-04 15:46:33 -0300

    AnkuLua daemon update

---
+++ ama_daemon/instances_run.bat
@@ -0,0 +1,4 @@
+@echo off
+.\tools\lua5.1.exe .\tools\install.luac -k --con=instances  --adb=adb
+set /p temp=""
+
---
+++ ama_daemon/tools/instances.bat
@@ -0,0 +1,17 @@
+@echo off
+.\tools\adb connect 127.0.0.1:6555
+
+rem itools simulator
+.\tools\adb connect 127.0.0.1:54001
+
+rem BS multiple instances
+.\tools\adb connect 127.0.0.1:5555
+.\tools\adb connect 127.0.0.1:6666
+.\tools\adb connect 127.0.0.1:7777
+.\tools\adb connect 127.0.0.1:9999
+
+rem wait for connect ready
+ping 127.0.0.1 -n 9 > nul
+
+
+
