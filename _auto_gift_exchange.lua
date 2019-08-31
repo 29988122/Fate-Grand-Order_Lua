fa0a158b7c113878f18a64c3ad04a7c4b150727c
TryBane <trybane@gmail.com>
2019-08-31 07:49:56 -0400

    Fixed issues and added a stop mechanic

--- _auto_gift_exchange.lua
+++ _auto_gift_exchange.lua
@@ -2,20 +2,20 @@
 xOffset = 0
 yOffset = 0
 
-xDifferential = getAppUsableScreenSize():getX() / 2560
-yDifferential = getAppUsableScreenSize():getY() / 1440
+xDifferential = getAppUsableScreenSize():getX() / 1280
+yDifferential = getAppUsableScreenSize():getY() / 720
 
 if yDifferential > xDifferential then
-    yOffset = ( getAppUsableScreenSize():getY() - ( xDifferential * 1440 ) ) / xDifferential / 2
+    yOffset = ( getAppUsableScreenSize():getY() - ( xDifferential * 720 ) ) / xDifferential / 2
     Settings:setCompareDimension(true,1280)
-    Settings:setScriptDimension(true,2560)
+    Settings:setScriptDimension(true,1280)
 elseif yDifferential < xDifferential then
-    xOffset = ( getAppUsableScreenSize():getX() - ( yDifferential * 2560 ) ) / yDifferential / 2
+    xOffset = ( getAppUsableScreenSize():getX() - ( yDifferential * 1280 ) ) / yDifferential / 2
     Settings:setCompareDimension(false,720)
-    Settings:setScriptDimension(false,1440)
+    Settings:setScriptDimension(false,720)
 else
     Settings:setCompareDimension(true,1280)
-    Settings:setScriptDimension(true,2560)
+    Settings:setScriptDimension(true,1280)
 end
 
 setImmersiveMode(true)
@@ -25,15 +25,14 @@ setImagePath(dir .. "image_EN")
 -- consts
 local SpinClick = Location(417 + xOffset, 430 + yOffset)
 local FinishedLotteryBoxRegion = Region(177 + xOffset, 411 + yOffset, 258, 151)
+local FullPresentBoxRegion = Region(640 + xOffset, 360 + yOffset, 640, 360)
 local ResetClick = Location(1100 + xOffset, 240 + yOffset)
 local ResetConfirmationClick = Location(837 + xOffset, 561 + yOffset)
 local ResetCloseClick = Location(635 + xOffset, 560 + yOffset)
 
 -- script
 local function spin()
-	for i = 1, 10 do
-		click(SpinClick)
-	end
+	continueClick(SpinClick,620)
 end
 
 local function reset()
@@ -50,6 +49,8 @@ end
 while(true) do
 	if FinishedLotteryBoxRegion:exists("lottery.png") then
 		reset()
+	elseif FullPresentBoxRegion:exists("StopGifts.png") then
+		scriptExit("Present Box Full")
 	else
 		spin()
 	end
