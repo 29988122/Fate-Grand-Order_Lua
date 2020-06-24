c5f4e8bdd1546e09a6c2697a5b692d8eb72eb142
forgehe <liao098@gmail.com>
2020-06-24 15:08:59 -0700

    add EN to use repeat feature in Free Quests

--- regular.lua
+++ regular.lua
@@ -157,7 +157,7 @@ local function Result()
 	wait(1)
 
 	--Only for JP currently. Searches for the Continue option after select Free Quests
-	if GameRegion == "JP" and game.CONTINUE_REGION:exists(GeneralImagePath .. "confirm.png") then
+	if (GameRegion == "JP" or GameRegion == "EN") and game.CONTINUE_REGION:exists(GeneralImagePath .. "confirm.png") then
 		IsContinuing = 1 -- Needed to show we don't need to enter the "StartQuest" function
 	
 		-- Pressing Continue option after completing a quest, reseting the state as would occur in "Menu" function
